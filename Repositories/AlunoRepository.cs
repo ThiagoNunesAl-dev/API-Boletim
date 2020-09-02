169e23fdbc90f10562811236fb3ccc87ee9c3c19
Thiago <thiagonunesal1307@gmail.com>
2020-09-01 21:26:19 -0300

    atividade finalizada

---
+++ Repositories/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Boletim.Domains;
+using API_Boletim.Interfaces;
+
+namespace API_Boletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
